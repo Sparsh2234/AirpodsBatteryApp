bbe67bd20a3c14c8f4e6791514a88aced1f93db0
Sparsh Pai <sparshpai@v1040-wn-rt-a-43-109.campus-dynamic.uwaterloo.ca>
2024-03-05 02:15:55 -0500

    setup

---
+++ AirpodsBatteryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirpodsBatteryApp
+//
+//  Created by Sparsh Pai on 2024-03-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
